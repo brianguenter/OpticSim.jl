7387db98e440824cbcd4b4a89b97ba83e23827fe
brianguenter <1brianguenter@gmail.com>
2022-10-23 12:49:00 -0700

    changed deploy url for docs to brianguenter instead of microsoft.

--- docs/make.jl
+++ docs/make.jl
@@ -44,7 +44,7 @@ makedocs(
 )
 
 deploydocs(
-    repo = "github.com/microsoft/OpticSim.jl.git",
+    repo = "github.com/brianguenter/OpticSim.jl.git",
     devbranch = "main",
     push_preview = true,
 )
