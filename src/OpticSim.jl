299abf24c77bb7a0d82072bc749b70e4e3d42e58
brianguenter <1brianguenter@gmail.com>
2025-02-23 11:23:02 -0800

    moved vis into its own directory with the aim of making it an independent package.
    moved examples direcotry out of OpticSim because examples has dependencies on Vis.

--- src/OpticSim.jl
+++ src/OpticSim.jl
@@ -29,7 +29,7 @@ include("Data/Data.jl")
 include("Geometry/Geometry.jl")
 include("Optical/Optical.jl")
 include("RepeatingStructures/Repeat.jl")
-include("Examples/Examples.jl")
+
 
 #initialize these caches here so they will get the correct number of threads from the load time environment, rather than the precompile environment. The latter happens if the initialization happens in the const definition. If the precompile and load environments have different numbers of threads this will cause an error.
 function __init__()
