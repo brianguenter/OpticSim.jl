724e1af223bab51aa42e138106d53dee1a37f261
brianguenter <1brianguenter@gmail.com>
2025-10-09 14:13:16 -0700

    added PolarizationRay struct and methods based on OpticalRay code

--- src/Optical/Polarization/PolarizationRay.jl
+++ src/Optical/Polarization/PolarizationRay.jl
@@ -3,31 +3,31 @@
 # See LICENSE in the project root for full license information.
 
 """
-    OpticalRay{T,N} <: AbstractRay{T,N}
+    PolarizationRay{T,N} <: AbstractRay{T,N}
 
 Ray incorporating polarization information.
 
-**NOTE**: we use monte carlo integration to get accurate results on the detector, this means that all rays essentially hit the detector with power = 1 and some rays are thrown away at any interface to correctly match the reflection/transmission at that interface. For inspection purposes we also track the 'instantaneous' power of the ray in the `power` field of the `OpticalRay`.
+**NOTE**: we use monte carlo integration to get accurate results on the detector, this means that all rays essentially hit the detector with power = 1 and some rays are thrown away at any interface to correctly match the reflection/transmission at that interface. For inspection purposes we also track the 'instantaneous' power of the ray in the `power` field of the `PolarizationRay`.
 
 ```julia
 PolarizationRay(ray::Ray{T,N}, power::T, wavelength::T, opl=zero(T))
-OpticalRay(origin::SVector{N,T}, direction::SVector{N,T}, power::T, wavelength::T, opl=zero(T))
+PolarizationRay(origin::SVector{N,T}, direction::SVector{N,T}, power::T, wavelength::T, opl=zero(T))
 ```
 
 Has the following accessor methods:
 ```julia
-ray(r::OpticalRay{T,N}) -> Ray{T,N}
-direction(r::OpticalRay{T,N}) -> SVector{N,T}
-origin(r::OpticalRay{T,N}) -> SVector{N,T}
-power(r::OpticalRay{T,N}) -> T
-wavelength(r::OpticalRay{T,N}) -> T
-pathlength(r::OpticalRay{T,N}) -> T
-sourcepower(r::OpticalRay{T,N}) -> T
-nhits(r::OpticalRay{T,N}) -> Int
-sourcenum(r::OpticalRay{T,N}) -> Int
+ray(r::PolarizationRay{T,N}) -> Ray{T,N}
+direction(r::PolarizationRay{T,N}) -> SVector{N,T}
+origin(r::PolarizationRay{T,N}) -> SVector{N,T}
+power(r::PolarizationRay{T,N}) -> T
+wavelength(r::PolarizationRay{T,N}) -> T
+pathlength(r::PolarizationRay{T,N}) -> T
+sourcepower(r::PolarizationRay{T,N}) -> T
+nhits(r::PolarizationRay{T,N}) -> Int
+sourcenum(r::PolarizationRay{T,N}) -> Int
 ```
 """
-struct OpticalRay{T,N} <: AbstractRay{T,N}
+struct PolarizationRay{T,N} <: AbstractRay{T,N}
     ray::Ray{T,N}
     power::T
     wavelength::T
@@ -36,37 +36,37 @@ struct OpticalRay{T,N} <: AbstractRay{T,N}
     sourcepower::T
     sourcenum::Int
 
-    function OpticalRay(ray::Ray{T,N}, power::T, wavelength::T; opl::T=zero(T), nhits::Int=0, sourcenum::Int=0, sourcepower::T=power) where {T<:Real,N}
+    function PolarizationRay(ray::Ray{T,N}, power::T, wavelength::T; opl::T=zero(T), nhits::Int=0, sourcenum::Int=0, sourcepower::T=power) where {T<:Real,N}
         return new{T,N}(ray, power, wavelength, opl, nhits, sourcepower, sourcenum)
     end
 
-    function OpticalRay(origin::SVector{N,T}, direction::SVector{N,T}, power::T, wavelength::T; opl::T=zero(T), nhits::Int=0, sourcenum::Int=0, sourcepower::T=power) where {T<:Real,N}
+    function PolarizationRay(origin::SVector{N,T}, direction::SVector{N,T}, power::T, wavelength::T; opl::T=zero(T), nhits::Int=0, sourcenum::Int=0, sourcepower::T=power) where {T<:Real,N}
         return new{T,N}(Ray(origin, normalize(direction)), power, wavelength, opl, nhits, sourcepower, sourcenum)
     end
 
-    function OpticalRay(origin::AbstractArray{T,1}, direction::AbstractArray{T,1}, power::T, wavelength::T; opl::T=zero(T), nhits::Int=0, sourcenum::Int=0, sourcepower::T=power) where {T<:Real}
+    function PolarizationRay(origin::AbstractArray{T,1}, direction::AbstractArray{T,1}, power::T, wavelength::T; opl::T=zero(T), nhits::Int=0, sourcenum::Int=0, sourcepower::T=power) where {T<:Real}
         @assert length(origin) == length(direction) "origin (dimension $(length(origin))) and direction (dimension $(length(direction))) vectors do not have the same dimension"
         N = length(origin)
         return new{T,N}(Ray(SVector{N,T}(origin), normalize(SVector{N,T}(direction))), power, wavelength, opl, nhits, sourcepower, sourcenum)
     end
 
     # Convenience constructor. Not as much typing
-    OpticalRay(ox::T, oy::T, oz::T, dx::T, dy::T, dz::T; wavelength=0.55) where {T<:Real} = OpticalRay(SVector{3,T}(ox, oy, oz), SVector{3,T}(dx, dy, dz), one(T), T(wavelength)) #doesn't have to be inside struct definition but if it is then VSCode displays hover information. If it's outside the struct definition it doesn't.
+    PolarizationRay(ox::T, oy::T, oz::T, dx::T, dy::T, dz::T; wavelength=0.55) where {T<:Real} = PolarizationRay(SVector{3,T}(ox, oy, oz), SVector{3,T}(dx, dy, dz), one(T), T(wavelength)) #doesn't have to be inside struct definition but if it is then VSCode displays hover information. If it's outside the struct definition it doesn't.
 end
-export OpticalRay
+export PolarizationRay
 
-ray(r::OpticalRay) = r.ray
-direction(r::OpticalRay{T,N}) where {T<:Real,N} = direction(ray(r))
-origin(r::OpticalRay{T,N}) where {T<:Real,N} = origin(ray(r))
-power(r::OpticalRay{T,N}) where {T<:Real,N} = r.power
-wavelength(r::OpticalRay{T,N}) where {T<:Real,N} = r.wavelength
-pathlength(r::OpticalRay{T,N}) where {T<:Real,N} = r.opl
-nhits(r::OpticalRay{T,N}) where {T<:Real,N} = r.nhits
-sourcepower(r::OpticalRay{T,N}) where {T<:Real,N} = r.sourcepower
-sourcenum(r::OpticalRay{T,N}) where {T<:Real,N} = r.sourcenum
+ray(r::PolarizationRay) = r.propagation_vector
+direction(r::PolarizationRay{T,N}) where {T<:Real,N} = direction(ray(r))
+origin(r::PolarizationRay{T,N}) where {T<:Real,N} = origin(ray(r))
+power(r::PolarizationRay{T,N}) where {T<:Real,N} = r.power
+wavelength(r::PolarizationRay{T,N}) where {T<:Real,N} = r.wavelength
+pathlength(r::PolarizationRay{T,N}) where {T<:Real,N} = r.opl
+nhits(r::PolarizationRay{T,N}) where {T<:Real,N} = r.nhits
+sourcepower(r::PolarizationRay{T,N}) where {T<:Real,N} = r.sourcepower
+sourcenum(r::PolarizationRay{T,N}) where {T<:Real,N} = r.sourcenum
 export ray, power, wavelength, pathlength, nhits, sourcepower, sourcenum
 
-function Base.print(io::IO, a::OpticalRay{T,N}) where {T,N}
+function Base.print(io::IO, a::PolarizationRay{T,N}) where {T,N}
     println(io, "$(rpad("Origin:", 25)) $(origin(a))")
     println(io, "$(rpad("Direction:", 25)) $(direction(a))")
     println(io, "$(rpad("Power:", 25)) $(power(a))")
@@ -79,6 +79,6 @@ function Base.print(io::IO, a::OpticalRay{T,N}) where {T,N}
     end
 end
 
-function Base.:*(a::Transform{T}, r::OpticalRay{T,N}) where {T,N}
-    return OpticalRay(a * ray(r), power(r), wavelength(r), opl=pathlength(r), nhits=nhits(r), sourcenum=sourcenum(r), sourcepower=sourcepower(r))
+function Base.:*(a::Transform{T}, r::PolarizationRay{T,N}) where {T,N}
+    return PolarizationRay(a * ray(r), power(r), wavelength(r), opl=pathlength(r), nhits=nhits(r), sourcenum=sourcenum(r), sourcepower=sourcepower(r))
 end
